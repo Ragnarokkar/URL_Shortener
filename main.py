460105e2a17dca5ce151671e8baf2e746304dd46
varun choudhry <varun.choudhry3@gmail.com>
2022-04-28 15:53:46 +0530

    Updated URL Validator, Removed demo code and my-form.html

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import random
 import re
 import string
 from flask import Flask, abort, jsonify, redirect, request, render_template
+from flask_cors import CORS, cross_origin
+import validators
+from validators import ValidationFailure
 app = Flask(__name__)
+CORS(app) #Added Cross Origin
 
 #Validate url
 def url_valid(url):
-    if url[:4] != 'http':
-        url = 'http://' + url
-    return re.match(regex, url) is not None
-
+    return validators.url(url)
+"""
+    prepared_request = PreparedRequest()
+    prepared_request.prepare_url(url, None)
+    return prepared_request.url    
+"""
+    
 #Placeholder URL Shorterner logic
 def shorten(url):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
@@ -20,21 +27,6 @@ def bad_request(message):
     response = jsonify({'message': message})
     response.status_code = 400
     return response
-    
-#Add a html page for testing
-@app.route('/')
-def my_form():
-    return render_template('my-form.html')
-#Get url from my-form.html for testing
-@app.route('/', methods=['POST'])
-def my_form_post():
-    url = request.form['text']
-    
-    if not url_valid(url):
-        return "Provided url is not valid."
-    shortened_url = shorten(url)
-    shortened[shortened_url] = url
-    return shortened_url
 
 #Method for POST API Call    
 @app.route('/shorten', methods=['POST'])
@@ -47,7 +39,6 @@ def shorten_url():
         return bad_request('Url not found.')
     
     url = request.json['url']
-
 
     if not url_valid(url):
         return bad_request('Url is not valid.')
@@ -69,14 +60,6 @@ def get_shortened(alias):
     
     return redirect(url, code=302)
 
-#Regular expression to validate url
-regex = re.compile(
-        r'^(?:http)s?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 #Varible to save url and shortened name as key-value pair        
 shortened = {}
 
