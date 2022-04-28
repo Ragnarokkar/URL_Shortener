2b082599eb0a3cf3333051b80d00820eede4aa75
varun choudhry <varun.choudhry3@gmail.com>
2022-04-28 14:47:33 +0530

    Added a html file which calls the api

---
+++ main.js
@@ -0,0 +1,34 @@
+//  main.js
+  
+// POST request using fetch()
+console.log("before");
+
+
+function doSomething() {
+    fetch("http://127.0.0.1:5000/shorten", {
+     
+    // Adding method type
+    method: "POST",
+     
+    // Adding body or contents to send
+    body: JSON.stringify({
+        url: document.getElementById("url").value,
+    }),
+     
+    // Adding headers to the request
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+ 
+// Converting to JSON
+.then(response => response.json())
+ 
+// Displaying results to console
+
+.then(function (json) {
+                var my_json = JSON.stringify(json, undefined, 2);
+                document.write(my_json);
+            });
+    return false;
+}
