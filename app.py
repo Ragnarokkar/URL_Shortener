62e84b60e14134517bb6abf6d2a70330e2123d24
varun choudhry <varun.choudhry3@gmail.com>
2022-04-28 21:25:27 +0530

    Updated to use text file instead of memory

--- app.py
+++ app.py
@@ -11,11 +11,32 @@ CORS(app) #Added Cross Origin to handle errors
 #Validate url
 def url_valid(url):
     return validators.url(url)
-
     
-#Placeholder URL Shorterner logic
+#Search the file for urls aready written    
+def search_file(url):
+    fo= open("data.txt","r")
+    for line in fo:
+        currentline = line.split(",")
+        if currentline[0]== url:
+            fo.close()
+            return currentline[1]
+    fo.close()        
+    return None
+    
+#Search the file for urls aready written    
+def search_file_short(url):
+    fo= open("data.txt","r")
+    for line in fo:
+        currentline = line.split(",")
+        if currentline[1]== url:
+            fo.close()
+            return currentline[0]
+    fo.close()        
+    return None      
+    
+#URL Shorterner logic
 def shorten(url):
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k = 7))
 
 #Function for bad API Calls    
 def bad_request(message):
@@ -38,10 +59,13 @@ def shorten_url():
 
     if not url_valid(url):
         return bad_request('Url is not valid.')
-
+    if search_file(url) is not None:
+        return jsonify({'shortened_url': search_file(url)}), 201
     shortened_url = shorten(url)
-    shortened[shortened_url] = url
-
+    #shortened[shortened_url] = url
+    fo= open("data.txt", "a")
+    fo.writelines(url+","+shortened_url+",\n")
+    fo.close()
     return jsonify({'shortened_url': shortened_url}), 201
 
 #Method to use GET API Call
@@ -52,7 +76,8 @@ def shorten_url_get():
 #Logic to redirect to the shortened url    
 @app.route('/<alias>', methods=['GET'])
 def get_shortened(alias):
-    url = shortened[alias]
+    if search_file_short(alias) is not None:
+        return redirect(search_file_short(alias), code=302)
     
     return redirect(url, code=302)
 
